5263dcbee5e22845a4b769e2869869219ab81b60
Conficker-V106 <137430505+Conficker-V106@users.noreply.github.com>
2023-07-29 13:11:28 +0500

    web site base

---
+++ Ahmed's Blog Web/static/js/home.js
@@ -0,0 +1,110 @@
+
+    var i = 0;
+    var j = 0
+    var txt = "Our greatest glory is not in never falling, but in rising every time we fall. ~Confucius";
+    var txt1 = "Ahmed Blog's"
+    var speed = 50;
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("quote").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+    function typeWritermain() {
+      
+      if (j < txt1.length) {
+        document.getElementById("name").innerHTML += txt1.charAt(j);
+        j++;
+        setTimeout(typeWritermain, speed);
+      }else{
+        typeWriter()
+      }
+    }
+
+
+
+    // dark light
+
+document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+var theme = "dark";
+  const root = document.querySelector(":root");
+  const container = document.getElementsByClassName("theme-container")[0];
+  const themeIcon = document.getElementById("theme-icon");
+  container.addEventListener("click", setTheme);
+  function setTheme() {
+    switch (theme) {
+      case "dark":
+        setLight();
+        theme = "light";
+        break;
+      case "light":
+        setDark();
+        theme = "dark";
+        break;
+    }
+  }
+  nav = document.getElementById("light1")
+  navtitle = document.getElementById("light2")
+  body = document.getElementById("light3")
+  textt = document.getElementById("light4")
+  function setLight() {
+    root.style.setProperty(
+      "--bs-dark",
+      "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+    );
+    nav.classList.remove("nav_bar")
+    nav.classList.add("nav_bar2")
+    navtitle.classList.remove("nav_title")
+    navtitle.classList.add("nav_title2")
+    body.classList.add("body2")
+    textt.classList.remove("bg-text")
+    textt.classList.add("bg-text2")
+    container.classList.remove("shadow-dark");
+    setTimeout(() => {
+      container.classList.add("shadow-light");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = sun;
+
+    
+
+  }
+
+
+  function setDark() {
+    root.style.setProperty("--bs-dark", "#212529");
+    container.classList.remove("shadow-light");
+    setTimeout(() => {
+      container.classList.add("shadow-dark");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = moon;
+    nav.classList.remove("nav_bar2")
+    nav.classList.add("nav_bar")
+    navtitle.classList.remove("nav_title2")
+    navtitle.classList.add("nav_title")
+    body.classList.remove("body2")
+    textt.classList.remove("bg-text2")
+    textt.classList.add("bg-text")
+  }
+
+
+  function myFunction() {
+    var x = document.getElementById("light1");
+    if (x.className === "topnav nav_bar") {
+      document.getElementById("light4").style.margin = "40px 0px 0px 0px"; 
+      x.className = "topnav responsive";
+      
+    } else {  
+      document.getElementById("light4").style.margin = "0px 0px 0px 0px"; 
+      x.className = "topnav nav_bar";
+    
+    }
+  }
---
+++ Ahmed's Blog Web/static/js/login.js
@@ -0,0 +1,83 @@
+document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+var theme = "dark";
+  const root = document.querySelector(":root");
+  const container = document.getElementsByClassName("theme-container")[0];
+  const themeIcon = document.getElementById("theme-icon");
+  container.addEventListener("click", setTheme);
+  function setTheme() {
+    switch (theme) {
+      case "dark":
+        setLight();
+        theme = "light";
+        break;
+      case "light":
+        setDark();
+        theme = "dark";
+        break;
+    }
+  }
+  body = document.getElementById("bodyid")
+  function setLight() {
+    root.style.setProperty(
+      "--bs-dark",
+      "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+    );
+    container.classList.remove("shadow-dark");
+    setTimeout(() => {
+      container.classList.add("shadow-light");
+      themeIcon.classList.remove("change");
+    }, 300);
+    body.classList.remove("lightdark1")
+    body.classList.add("lightdark2")
+    themeIcon.classList.add("change");
+    themeIcon.src = sun;
+    
+  }
+  function setDark() {
+    root.style.setProperty("--bs-dark", "#212529");
+    container.classList.remove("shadow-light");
+    setTimeout(() => {
+      container.classList.add("shadow-dark");
+      themeIcon.classList.remove("change");
+    }, 300);
+    body.classList.remove("lightdark2")
+    body.classList.add("lightdark1")
+    themeIcon.classList.add("change");
+    themeIcon.src = moon;
+    
+  }
+
+
+
+
+
+
+  const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+const loginLink = document.getElementById('login-link');
+const signupLink = document.getElementById('signup-link');
+
+loginLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  signupForm.style.display = 'none';
+  loginForm.style.display = 'block';
+
+  setTimeout(() => {
+    signupForm.style.opacity = 0;
+    loginForm.style.opacity = 1;
+  }, 10);
+});
+
+signupLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  loginForm.style.display = 'none';
+  signupForm.style.display = 'block';
+
+  setTimeout(() => {
+    loginForm.style.opacity = 0;
+    signupForm.style.opacity = 1;
+  }, 10);
+});
